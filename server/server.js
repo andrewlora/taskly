b04f67db27e4fef2ad545e52c53eee3536b09d4c
Andres Lora <jalc.andres.lora@gmail.com>
2024-04-18 09:52:43 -0700

    Added server with express, connection with mongodb and user route

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+
+import userRouter from "./routes/user.route.js";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use("/api/v1/users", userRouter);
+
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "not found" });
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
