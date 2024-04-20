7684791e2b09c03b59d00748eab54a12563331c3
Andres Lora <jalc.andres.lora@gmail.com>
2024-04-19 17:32:00 -0700

    Created template Create, Single, Tasks, Update Forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,14 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateTask from "./pages/CreateTask";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import SingleTask from "./pages/SingleTask";
+import Tasks from "./pages/Tasks";
+import UpdateTask from "./pages/UpdateTask";
 function App() {
   return (
     <BrowserRouter>
@@ -17,6 +21,10 @@ function App() {
         <Route path="/signUp" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-task" element={<CreateTask />} />
+          <Route path="/update-task" element={<UpdateTask />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/task/:id" element={<SingleTask />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/SingleTask.jsx
@@ -0,0 +1,5 @@
+const SingleTask = () => {
+  return <div>SingleTask</div>;
+};
+
+export default SingleTask;
